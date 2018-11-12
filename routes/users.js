fd3ec862b5ed24eb853dbc72b2e671044ca3af2b
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-11-12 16:56:17 -0300

    Fix rota delete user e criando getAll users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 module.exports = app => {
     const Users = app.db.models.Users;
 
+    app.get("/users", (req, res) => {
+        Users.findAll({})
+            .then(result => res.json(result))
+            .catch(error => {
+                res.status(412).json({ msg: error.message });
+            });
+    })
     app.get("/users/:id", (req, res) => {
         Users.findById(req.params.id, {
             attributes: ["id", "name", "email"]
@@ -11,7 +18,7 @@ module.exports = app => {
         });
     });
 
-    app.delete("/users:id", (req, res) => {
+    app.delete("/users/:id", (req, res) => {
         Users.destroy({ where: {id: req.params.id } })
             .then(result => res.sendStatus(204))
             .catch(error => {
